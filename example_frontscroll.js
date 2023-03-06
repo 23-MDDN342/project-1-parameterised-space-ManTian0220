884f99b66b6ab3a7b47c9706e09354a564e25602
ManTian0220 <100631769+ManTian0220@users.noreply.github.com>
2023-03-07 10:32:58 +1300

    starting point
    Create a monster

--- example_frontscroll.js
+++ example_frontscroll.js
@@ -1,20 +1,17 @@
 function draw_one_frame(cur_frac) {
-  let sun_size = canvasHeight/8;
-
-  noStroke();
-  fill(100, 100, 214);
-  rect(0, 0, width, height);
-
-  fill(255, 255, 0);
-  ellipse(width/2, height/2, sun_size);
-
-  fill(0, 200, 0);
-  rect(0, height/2, width, height/2);
-
-  stroke(0);
-  line(width/2, height/2, width/2, height);
-  line(0.40*width, height/2, 0.20*width, height);
-  line(0.60*width, height/2, 0.80*width, height);
+  let sun_size = canvasHeight/2;
+fill(100, 100, 214);
+ellipse(width/2,height/1,sun_size/0.5,sun_size/0.5);
+fill(255, 255, 0);
+circle(width/1.8,height/1.8,sun_size/12);
+fill(100, 100, 214);
+circle(width/1.8,height/1.8,sun_size/30);
+fill(255, 255, 0);
+circle(width/1.95,height/1.8,sun_size/12);
+fill(100, 100, 214);
+circle(width/1.95,height/1.8,sun_size/30);
+fill(255, 255, 0);
+ellipse(width/1.87,height/1.7,sun_size/12,sun_size/18);
 
   strokeWeight(10);
   let grid_points = [
@@ -25,18 +22,18 @@ function draw_one_frame(cur_frac) {
     1.00 * height
   ]
 
-  if (debugView) {
-    strokeWeight(1);
-    stroke(255, 0, 0);
-    for(let i=0; i<grid_points.length; i++) {
-      line(0, grid_points[i], width, grid_points[i]);
-    }
-  }
+   if (debugView) {
+     strokeWeight(1);
+     stroke(255, 0, 0);
+     for(let i=0; i<grid_points.length; i++) {
+       line(0, grid_points[i], width, grid_points[i]);
+     }
+   }
 
-  strokeWeight(2);
-  stroke(0);
-  for(let i=0; i<grid_points.length-1; i++) {
-    let cur_grid_line = map(cur_frac, 0, 1, grid_points[i], grid_points[i+1])
-    line(0, cur_grid_line, width, cur_grid_line);
-  }
+  // strokeWeight(2);
+  // stroke(0);
+  // for(let i=0; i<grid_points.length-1; i++) {
+  //   let cur_grid_line = map(cur_frac, 0, 1, grid_points[i], grid_points[i+1])
+  //   line(0, cur_grid_line, width, cur_grid_line);
+  // }
 }
